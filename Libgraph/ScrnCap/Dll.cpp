1278e3b30939f669355ffd9a820ef3cee1ddc2e0
rogerdpack <rogerpack2005@gmail.com>
2011-07-28 12:20:23 -0600

    and it's totally bwokded :(

--- Libgraph/ScrnCap/Dll.cpp
+++ Libgraph/ScrnCap/Dll.cpp
@@ -69,6 +69,114 @@ int g_cTemplates = sizeof(g_Templates) / sizeof(g_Templates[0]);
 // (in this case they only call through to default implementations).
 ////////////////////////////////////////////////////////////////////////
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define CreateComObject(clsid, iid, var) CoCreateInstance( clsid, NULL, CLSCTX_INPROC_SERVER, iid, (void **)&var);
+
+STDAPI AMovieSetupRegisterServer( CLSID   clsServer, LPCWSTR szDescription, LPCWSTR szFileName, LPCWSTR szThreadingModel = L"Both", LPCWSTR szServerType     = L"InprocServer32" );
+STDAPI AMovieSetupUnregisterServer( CLSID clsServer );
+
+STDAPI RegisterFilters( BOOL bRegister )
+{
+    HRESULT hr = NOERROR;
+    WCHAR achFileName[MAX_PATH];
+    char achTemp[MAX_PATH];
+    ASSERT(g_hInst != 0);
+
+    if( 0 == GetModuleFileNameA(g_hInst, achTemp, sizeof(achTemp))) 
+        return AmHresultFromWin32(GetLastError());
+
+    MultiByteToWideChar(CP_ACP, 0L, achTemp, lstrlenA(achTemp) + 1, 
+                       achFileName, NUMELMS(achFileName));
+  
+    hr = CoInitialize(0);
+    if(bRegister)
+    {
+        hr = AMovieSetupRegisterServer(CLSID_ScreenCapturer, wszName, achFileName, L"Both", L"InprocServer32");
+    }
+
+    if( SUCCEEDED(hr) )
+    {
+        IFilterMapper2 *fm = 0;
+        hr = CreateComObject( CLSID_FilterMapper2, IID_IFilterMapper2, fm );
+        if( SUCCEEDED(hr) )
+        {
+            if(bRegister)
+            {
+                IMoniker *pMoniker = 0;
+                REGFILTER2 rf2;
+                rf2.dwVersion = 1;
+                rf2.dwMerit = MERIT_DO_NOT_USE;
+                rf2.cPins = 1;
+                rf2.rgPins = &AMSPinScreenCap;
+                hr = fm->RegisterFilter(CLSID_ScreenCapturer, wszName, &pMoniker, &CLSID_VideoInputDeviceCategory, NULL, &rf2);
+            }
+            else
+            {
+                hr = fm->UnregisterFilter(&CLSID_VideoInputDeviceCategory, 0, CLSID_ScreenCapturer);
+            }
+        }
+
+      // release interface
+      //
+      if(fm)
+          fm->Release();
+    }
+
+    if( SUCCEEDED(hr) && !bRegister )
+        hr = AMovieSetupUnregisterServer( CLSID_ScreenCapturer );
+
+    CoFreeUnusedLibraries();
+    CoUninitialize();
+    return hr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+STDAPI DllRegisterServer()
+{
+    return RegisterFilters(TRUE);
+}
+
+STDAPI DllUnregisterServer()
+{
+    return RegisterFilters(FALSE);
+}
+
+
+
+
+
+
+/*
 STDAPI DllRegisterServer()
 {
     return AMovieDllRegisterServer2( TRUE );
@@ -78,6 +186,7 @@ STDAPI DllUnregisterServer()
 {
     return AMovieDllRegisterServer2( FALSE );
 }
+*/
 
 //
 // DllEntryPoint
