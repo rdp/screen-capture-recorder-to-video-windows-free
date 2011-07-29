11eeee9d19b3b02d49b3fb8d11e8e9f068ece704
rogerdpack <rogerpack2005@gmail.com>
2011-07-29 10:25:07 -0600

    can read

---
+++ pushsource/Debug/reg.c
@@ -0,0 +1,19 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(char** args) {
+  
+//  HKEY_LOCAL_MACHINE\SOFTWARE\os_screen_capture
+    HKEY hKey;
+    // Check that UK Phone Codes    is installed
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+       "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Perflib",
+        0, KEY_READ, &hKey) != ERROR_SUCCESS)
+    {
+        printf("ossc none");
+        return;
+    } else {
+      printf("ossc yes");  
+    }
+  
+}
