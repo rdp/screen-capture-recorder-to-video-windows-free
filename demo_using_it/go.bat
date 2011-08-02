1200e30443dcfc2361f59aea85427841061138ff
rogerdpack <rogerpack2005@gmail.com>
2011-08-01 18:49:09 -0600

    new coords

--- demo_using_it/go.bat
+++ demo_using_it/go.bat
@@ -1,1 +1,1 @@
-mplayer -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf hqdn3d=0:1:4:4,pp=hb:y/vb:y,scale=1280:-10:0:0:2  upconvert_from_screen_me2.avs
+mplayer -geometry 50%x40% -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf hqdn3d=0:1:4:4,pp=hb:y/vb:y,scale=1024:-10:0:0:2  upconvert_from_screen_me2.avs
---
+++ demo_using_it/go_raw.bat
@@ -0,0 +1,2 @@
+@rem mplayer -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf pp=hb:y/vb:y,hqdn3d=0:1:4:4,scale=1280:-10:0:0:2  upconvert_from_screen_me2.avs
+mplayer   upconvert_from_screen_me2.avs
