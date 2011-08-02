a69b87459ab69895dffd775821b5b6635189e7a7
rogerdpack <rogerpack2005@gmail.com>
2011-08-02 10:06:17 -0600

    dunno

--- demo_using_it/go.bat
+++ demo_using_it/go.bat
@@ -1,1 +1,2 @@
-mplayer -geometry 50%x40% -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf hqdn3d=0:1:4:4,pp=hb:y/vb:y,scale=1024:-10:0:0:2  upconvert_from_screen_me2.avs
+@rem mplayer -geometry 50%x40% -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf hqdn3d=4:3:6:4,pp=hb:y/vb:y,scale=1280:-10:0:0:2  upconvert_from_screen_me2.avs
+mplayer -geometry 50%x40% -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf scale=1680:-10:0:0:10,hqdn3d  upconvert_from_screen_me2.avs
