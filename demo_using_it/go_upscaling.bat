5174486d6647c2e86c97104ffa3b47f609c0c443
rogerdpack <rogerpack2005@gmail.com>
2011-08-09 09:49:35 -0600

    uh like it fer yuh tube

--- demo_using_it/go_upscaling.bat
+++ demo_using_it/go_upscaling.bat
@@ -1,2 +1,7 @@
-@rem mplayer -geometry 50%x40% -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf hqdn3d=4:3:6:4,pp=hb:y/vb:y,scale=1280:-10:0:0:2  upconvert_from_screen_me2.avs
-mplayer -nocache -geometry 50%x40% -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf hqdn3d=4:3:6:4,scale=1680:-10:0:0:10  upconvert_from_screen_me2.avs
+mplayer -geometry 50%x40% -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf hqdn3d=7:7:7:7,scale=1280:-10:0:0:3  upconvert_from_screen_me2.avs
+
+@rem 0:1:4:4 for DVD LOL
+@rem pp=hb:y/vb:y,
+
+
+@rem mplayer autoq=100 -nocache -geometry 50%x40% -sws 9 -ssf ls=75.0 -ssf cs=7.0 -vf nr=1000,scale=1680:-10:0:0:4  upconvert_from_screen_me2.avs
