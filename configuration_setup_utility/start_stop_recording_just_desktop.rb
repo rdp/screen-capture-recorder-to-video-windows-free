2b7b14c7a20ade8c4166f9de5a43317e013ab61d
rogerdpack <rogerpack2005@gmail.com>
2011-09-06 16:27:25 -0600

    bundle ffmpeg, make more generic

--- configuration_setup_utility/start_stop_recording_just_desktop.rb
+++ configuration_setup_utility/start_stop_recording_just_desktop.rb
@@ -1,12 +1,13 @@
 require 'jruby-swing-helpers/swing_helpers'
-
+# use like ".mp3" "audio=xxx" if desired
+ENV['PATH'] = 'ffmpeg\bin;' + ENV['PATH']
 include SwingHelpers
 
 seconds = SwingHelpers.get_user_input("Seconds to record for?", 60)
 p "doing #{seconds} seconds"
 file = JFileChooser.new_nonexisting_filechooser_and_go 'select_file_to_write_to', File.dirname(__FILE__)
 
-file += ".mp4" unless file =~ /\./ # add extension for them...
+file += (ARGV[0] || ".mp4" ) unless file =~ /\./ # add extension for them...
 
 if File.exist? file
   got = JOptionPane.show_select_buttons_prompt "overwrite #{file}?", :yes => "yes", :no => "cancel"
@@ -16,7 +17,8 @@ end
 
 #got = JOptionPane.show_select_buttons_prompt 'Select start to start', :yes => "start", :no => "stop"
 #raise unless got == :yes
-c = "ffmpeg -f dshow -i video=screen-capture-recorder -r 20 -t #{seconds} \"#{file}\""
+device = ARGV[1] || "video=screen-capture-recorder"
+c = "ffmpeg -f dshow -i #{device} -r 20 -t #{seconds} \"#{file}\""
 puts c
 p c
 system c
