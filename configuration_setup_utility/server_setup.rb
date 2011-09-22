bd9421ba665815232b2ee5ceac1d725368233a7f
rogerdpack <rogerpack2005@gmail.com>
2011-09-22 13:01:41 -0600

    init

---
+++ configuration_setup_utility/server_setup.rb
@@ -0,0 +1,8 @@
+require 'jruby-swing-helpers/swing_helpers'
+include SwingHelpers
+require 'java'
+
+names = ['one, two, three']
+p DropDownSelector.new(nil, names, "Select audio device to stream:").go
+
+#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=44100}'
