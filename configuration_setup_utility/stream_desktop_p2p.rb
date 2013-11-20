7621f60d9b1c4569906a793e2bee86cba6346529
rogerdpack <rogerpack2005@gmail.com>
2013-11-20 07:35:28 -0700

    seralize stream to url/save it off

--- configuration_setup_utility/stream_desktop_p2p.rb
+++ configuration_setup_utility/stream_desktop_p2p.rb
@@ -2,13 +2,13 @@
 
 template=
 %!-------------Stream Desktop--------------
-"Enter your output streaming url, like udp://236.0.0.1:2000:fake_name (mplayer will receive from the same address)"
+"Enter your output streaming url, like udp://236.0.0.1:2000 (mplayer will receive from the same address):"
 "   udp://hostname:port:fake_name"
-"   or for multicast something like udp://236.0.0.1:2000"
-"  You can receive the stream via some player, ex: "
-" mplayer -cache 1024 -demuxer +mpegts -framedrop -benchmark -geometry 50:50 udp://236.0.0.1:2000"
-[udp://:stream_url,width=700, height=20px]
-[                                        ]
+"   or for multicast something like udp://236.0.0.1:2000:"
+"  You can receive the stream via some player, ex::"
+" mplayer -demuxer +mpegts -framedrop -benchmark udp://236.0.0.1:2000:"
+[udp://localhost:2000:stream_url,width=700, height=20px]
+[                                                      ]
 [Start/Stop:start_stop_button] "status:status_text,width=50chars"
 !
 
@@ -16,13 +16,17 @@ template=
 
 puts template
 require 'common_recording.rb'
-
+@storage = Storage.new('stream_desktop_p2p')
+@storage.set_default('stream_to_url', 'udp://236.0.0.1:2000')
 @status = :stopped
 
 @frame = ParseTemplate.new.parse_setup_string template
+@frame.elements[:stream_url].text=@storage['stream_to_url']
 @frame.elements[:start_stop_button].on_clicked {
   if @status == :stopped
-     c = "ffmpeg -f dshow  -framerate 5 -i video=screen-capture-recorder -vf scale=1280:720 -vcodec libx264 -pix_fmt yuv420p -tune zerolatency -preset ultrafast -qp 10 -g 30 -f mpegts #{@frame.elements[:stream_url].text.strip}"
+       extra_options="-qp 10 -g 30"
+      #s extra_options="-g 30"
+       c = "ffmpeg -f dshow  -framerate 5 -i video=screen-capture-recorder -vf scale=1280:720 -vcodec libx264 -pix_fmt yuv420p -tune zerolatency -preset ultrafast #{extra_options} -f mpegts #{@frame.elements[:stream_url].text.strip}"
 	   puts "starting #{c}"
 	   @current_process = IO.popen(c, "w") # jruby friendly :P
 	   Thread.new { 
@@ -44,3 +48,11 @@ def update_ui
 end
 
 update_ui # init
+
+@frame.after_closed {
+  if @status == :running
+    SimpleGuiCreator.show_blocking_message_dialog "warning, shutting down streaming with window closing"
+   end
+   @storage['stream_to_url']=@frame.elements[:stream_url].text
+
+}
