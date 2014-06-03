972bb96f4ce9bb1ff613332848586ca5d83b5529
rogerdpack <rogerpack2005@gmail.com>
2014-06-03 16:31:00 -0600

    move audio to top of options pane since it gets lost otherwise, cleanup readme, prep for release

--- configuration_setup_utility/record_with_buttons_options_pane.rb
+++ configuration_setup_utility/record_with_buttons_options_pane.rb
@@ -34,6 +34,7 @@ def show_options_frame
   template = <<-EOL
   ------------ Recording Options -------------
   [Close This Options Window :close]
+  [Select audio devices:select_new_audio] " #{remove_quotes(audio_device_names_or_nil || 'none selected')} :audio_name" 
   [Select video device:select_new_video] " #{remove_quotes(video_device_name_or_nil || 'none selected')} :video_name"
   EOL
   
@@ -46,7 +47,6 @@ def show_options_frame
   end
   
   template += <<-EOL  
-  [Select audio devices:select_new_audio] " #{remove_quotes(audio_device_names_or_nil || 'none selected')} :audio_name" 
   [✓:record_to_file] "Save to file"  "an awesome file location!!!!!:save_to_dir_text" [ Set directory :set_directory]
   [✓:auto_display_files] "Automatically reveal files in windows explorer after each recording:auto_display_files_text"
   [✓:stream_to_url_checkbox] "Stream to url:"  "Specify url first!!!!!!!!!:url_stream_text" [ Set streaming options : set_stream_url ]
--- innosetup_installer_options.iss
+++ innosetup_installer_options.iss
@@ -1,4 +1,4 @@
-#define AppVer "0.12.5"
+#define AppVer "0.12.6"
 
 #define AppName "Screen Capturer Recorder"
 ; AppId === AppName by default BTW
