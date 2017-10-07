f5f957410716a6f76f03066fb3f9cbf70b6138e4
rogerdpack <rogerpack2005@gmail.com>
2017-10-07 01:27:26 -0600

    it was totally broken waah

--- configuration_setup_utility/generic_run_rb.bat
+++ configuration_setup_utility/generic_run_rb.bat
@@ -1,5 +1,5 @@
 @rem echo Loading... we have splash for this now
-
+echo on
 @setlocal
 @rem disable any local rubyopt settings, just in case...
 @set RUBYOPT=
@@ -8,7 +8,7 @@
 @set PATH=%WINDIR%\syswow64;%PATH%
 @rem add in JAVA_HOME just for fun/in case
 @set PATH=%PATH%;%JAVA_HOME%\bin
-@call java -version > NUL 2>&1 || echo you need to install java JRE first please install it from java.com then run again && java -version && pause && GOTO INSTALL_JAVA
+@call java -version > NUL 2>&1 || GOTO INSTALL_JAVA
 
 @rem success path
 @java -splash:audio.jpg -jar jruby-complete-1.7.12.jar --1.9 -I. %*
@@ -16,6 +16,7 @@
 @GOTO DONE
 
 :INSTALL_JAVA
+echo you need to install java JRE first please install it from java.com then run again && pause
 start http://java.com
 
 :DONE
