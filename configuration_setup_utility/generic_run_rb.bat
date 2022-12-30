f35dd55e576b13e44caa58b6a9024226735c0e7e
rogerdpack <rogerpack2005@gmail.com>
2022-12-30 00:19:58 -0700

    actually work in jdk > 8?

--- configuration_setup_utility/generic_run_rb.bat
+++ configuration_setup_utility/generic_run_rb.bat
@@ -1,19 +1,41 @@
 @rem echo Loading... we have splash for this these days...
-
-@setlocal
-@rem disable any local rubyopt settings, just in case...
-@set RUBYOPT=
+@echo on
+setlocal
+@rem disable any other local rubyopt settings, just in case...
+set RUBYOPT=
 
 @rem have 32 bit java available on 64 bit machines. Yikes java, yikes.
-@set PATH=%WINDIR%\syswow64;%PATH%
+set PATH=%WINDIR%\syswow64;%PATH%
 @rem add in JAVA_HOME just for fun/in case
-@set PATH=%PATH%;%JAVA_HOME%\bin
-@call java -version > NUL 2>&1 || GOTO INSTALL_JAVA
+set PATH=%PATH%;%JAVA_HOME%\bin
+call java -version > NUL 2>&1 || GOTO INSTALL_JAVA
 
-@rem success path
-@java -splash:audio.jpg -jar jruby-complete-9.2.13.0.jar -I. %*
+rem success path now
 
-@GOTO DONE
+rem parse out java version, which is a mess LOL https://stackoverflow.com/a/49688919/32453
+rem Parses x out of 1.x; for example 8 out of java version 1.8.0_xx
+rem Otherwise, parses the major version; 9 out of java version 9-ea
+set JAVA_VERSION=0
+for /f "tokens=3" %%g in ('java -version 2^>^&1 ^| findstr /i "version"') do (
+  set JAVA_VERSION=%%g
+)
+set JAVA_VERSION=%JAVA_VERSION:"=%
+for /f "delims=.-_ tokens=1-2" %%v in ("%JAVA_VERSION%") do (
+  if /I "%%v" EQU "1" (
+    set JAVA_VERSION=%%w
+  ) else (
+    set JAVA_VERSION=%%v
+  )
+)
+
+rem newer versions need help to actually send signals to child processes?
+if %JAVA_VERSION% LSS 9 (
+     java  -splash:audio.jpg -jar jruby-complete-9.2.13.0.jar -I. %*
+   ) else (
+     java  --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED -splash:audio.jpg -jar jruby-complete-9.2.13.0.jar -I. %*
+   )
+
+GOTO DONE
 
 :INSTALL_JAVA
 msg "%username%" you need to install java first please install it from java.com then run again
